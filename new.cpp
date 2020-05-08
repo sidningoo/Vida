507192606585c2d40ad38dac6a49c036145fafaa
sdningoo <62166148+sdningoo@users.noreply.github.com>
2020-05-08 19:24:32 +0800

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
 using namespace std;
+int health = 5;
+int wealth = 5;
+int happiness = 5;
+
 
 int Gamedeveloper(){}
-int teacher(){}
+
+
+int teacher(){
+  int counter = 0;
+  while (health < 1 || wealth < 1 || happiness < 1){
+    char questioninput;
+    if (counter == 0){
+      cout << "Question 1" << endl << "Where do you want to teach?" << endl;
+      cout << "     A.  University" << endl;
+      cout << "     B.  High School" <<endl;
+      cout << "     C.  Middle School" <<endl;
+      cout << "     D.  Primary School" << endl;
+
+      if (questioninput == A){
+        wealth += 2;
+        cout << "Your Wealth increased by 2!" << endl;
+      }
+      if (questioninput == A){
+        happiness += 2;
+      }
+      if (questioninput == A){
+        happiness += 1;
+      }
+      if (questioninput == A){
+        health += 1;
+      }
+      cout << "Health = " << health << " / 10" << endl;
+      cout << "Wealth = " << health << " / 10" << endl;
+      cout << "Happiness = " << health << " / 10" << endl;
+    }
+  }
+}
+
+
 int actor(){}
+
+
 int doctor(){}
+
 
 int main(){
   string name;
@@ -15,7 +55,7 @@ int main(){
   cout << endl<< "Why Hello " << name << endl;
   cout << "You are about to start your journey, pick your career" << endl;
   cout << "     A.  Game developer" << endl;
-  cour << "     B.  Teacher" <<endl;
+  cout << "     B.  Teacher" <<endl;
   cout << "     C.  Doctor" <<endl;
   cout << "     D.  Actor" << endl;
   cin << pickcareer;
